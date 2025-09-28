bd7e275178219d1a21f15fa03e34867cf3498c95
Abu <fypabdullah@gmail.com>
2025-09-29 03:00:06 +0530

    fix(workflow): fixed issue with TF vars

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,11 @@
 variable "aws_region" {
   description = "AWS region to deploy into"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "image_tag" {
   description = "Docker image tag to deploy"
   type        = string
+  default     = "latest"
 }
